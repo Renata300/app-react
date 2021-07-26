04d150359e7d366a19d954934994391b5f2a255d
Renata Mottin <renata_mottin@dellteam.com>
2021-07-26 08:48:57 -0300

    Finished to create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Button, Container, Form, Grid, Header, Icon, Segment, Statistic } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
-import ButtonSaveOrCancel from './components/ButtonSaveOrCancel';
 import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EnteryLine from './components/EnteryLine';
 import MainHeader from './components/MainHeader';
 import NewEnteryForm from './components/NewEnteryForm';
 
@@ -9,59 +10,14 @@ function App() {
   return (
     <Container>
       <MainHeader title='My Project'/>
-      <Statistic size='small'>
-        <Statistic.Label>Your Balance:</Statistic.Label>
-        <Statistic.Value>2000</Statistic.Value>
-      </Statistic>
-      <Segment textAlign='center'>
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column>
-              <DisplayBalance title='Income' value='1050.50' color='green'/>
-            </Grid.Column>
-            <Grid.Column>
-              <DisplayBalance title='Expenses' value='620.45' color='red'/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <DisplayBalance title='Your Balance:'value='2000' size='small'/>
+
+      <DisplayBalances/>
       <MainHeader title='History' type='h3'/>
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$100,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered/> 
-              <Icon name='trash'bordered/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color='green'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Anything</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$10,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered/> 
-              <Icon name='trash'bordered/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color='blue'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Everything</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$1000,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered/> 
-              <Icon name='trash'bordered/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      
+      <EnteryLine description='Income' value='$10.00'/>
+      <EnteryLine description='Expense' value='$100.00' isExpense/>
+      
       <MainHeader title='Add new transaction' type='h3'/> 
       <NewEnteryForm />
     </Container>
