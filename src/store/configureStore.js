a9fe6186e24e3647bdd13a7d67529928ca9aad05
Renata Mottin <renata_mottin@dellteam.com>
2021-08-05 08:55:28 -0300

    Finished Devtools implementation

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import entriesReducer from "../reducers/entries.reducers";
+import modalReducer from '../reducers/modals.reducers'
 
 const configureStore = () => {
   return createStore(
     combineReducers({
       entries: entriesReducer,
+      modals: modalReducer,
     }), composeWithDevTools()
   );
 };
